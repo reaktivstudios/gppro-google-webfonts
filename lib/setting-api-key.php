5e15092cc255239c071278c22f348d0611ffd215
Cory Webb <cory@corywebbmedia.com>
2019-04-19 15:40:02 -0500

    Add link to Google developer api key

--- lib/setting-api-key.php
+++ lib/setting-api-key.php
@@ -13,9 +13,17 @@
  * @return array
  */
 function gppro_google_webfonts_api_key( $settings ) {
+	$description = sprintf(
+		'%1$s <a href="%2$s" target="_blank">%3$s</a> %4$s.',
+		__( 'You must have a', 'gppro-google-webfonts' ),
+		esc_url( 'https://developers.google.com/fonts/docs/developer_api' ),
+		__( 'Google Fonts Developer API key', 'gppro-google-webfonts' ),
+		__( 'to access the Google Fonts feature', 'gppro-google-webfonts' )
+	);
+
 	$settings['gppro_google_webfonts_api_key'] = array(
 		'label'       => __( 'Google Fonts API Key', 'gppro-google-webfonts' ),
-		'description' => __( 'You must have a Google Fonts Developer API key to access the Google Fonts feature.', 'gppro-google-webfonts' ),
+		'description' => $description,
 		'default'     => '',
 		'type'        => 'text',
 		'section'     => 'settings',
